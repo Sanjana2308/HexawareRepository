01e637247a9f72e74c897043b3c3096f52dbc765
Sanjana Pandey <sanjanapandey2002@outlook.com>
2024-05-14 22:48:47 +0530

    Task 4 Completed

--- python.py
+++ python.py
@@ -69,6 +69,30 @@ def calculate_compound_interest():
 calculate_compound_interest()
 
 
+# Task 4:
+def check_account_balance():
+    accounts = {}
+    num_customers = int(input("Enter the number of customers: "))
+
+    for number in range(1, num_customers + 1):
+        print(f"Enter details for Customer {number} 🙂")
+        account_number = int(input("Enter your account number: "))
+        # Checking if customer has already entered account number or not
+        if account_number not in accounts:
+            balance = int(input("Enter your balance: "))
+            accounts[account_number] = balance
+        while True:
+            account_number = int(input("Enter your account number for validation: "))
+            if account_number in accounts:
+                break
+            else:
+                print("Warning! ⚠️  Wrong credentials! Try again 🙂")
+        print(f"Your account balance is ₹{accounts[account_number]}")
+        
+check_account_balance()
+
+
+
 
 
 
